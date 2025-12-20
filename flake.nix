a7d46017c0e7e22ad13f787d63b2e4f8120d54c8
Emil Stegmann <iouhase@Emils-MacBook-Pro.local>
2025-12-20 10:58:21 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+
+      nixpkgs.config.allowUnfree = true;	
+
+      environment.systemPackages =
+        [ 
+		pkgs.vim
+		pkgs.mkalias
+		pkgs.vscode
+		pkgs.firefox
+		#pkgs.spotify
+		pkgs.discord
+		pkgs.zip
+		pkgs.unzip
+        ];
+
+      homebrew = {
+	enable = true;
+
+	brews = [
+	  "mas"
+	];
+
+	casks = [
+	  "spotify"
+	  "prismlauncher"
+	];
+
+	masApps = {
+	  #"Yoink" = 457622435;
+	};
+
+	onActivation.cleanup = "zap";
+      };
+
+      system.primaryUser = "iouhase";
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#laptop
+    darwinConfigurations."laptop" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+	configuration 
+	nix-homebrew.darwinModules.nix-homebrew
+	{
+		nix-homebrew = {
+			enable = true;
+			enableRosetta = true;
+			user = "iouhase";
+		};
+	}
+	];
+    };
+  };
+}
