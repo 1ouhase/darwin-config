3407d6b911103bfdbee51d41ef0a0e0e91fd8267
Emil Stegmann <iouhase@Emils-MacBook-Pro.local>
2025-12-24 23:30:41 +0100

    split flake into flake and conf

--- configuration.nix
+++ configuration.nix
@@ -1,81 +1,87 @@
- let
-    configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+{ pkgs, config, ... }: {
+  # Nixpkgs configuration
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
-      nixpkgs.config.allowUnfree = true;	
+  # System packages
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.mkalias
+    pkgs.vscode
+    pkgs.firefox
+    pkgs.discord
+    pkgs.zip
+    pkgs.unzip
+    pkgs.neofetch
+  ];
 
-      environment.systemPackages =
-        [ 
-		pkgs.vim
-		pkgs.mkalias
-		pkgs.vscode
-		pkgs.firefox
-		#pkgs.spotify
-		pkgs.discord
-		pkgs.zip
-		pkgs.unzip
+  # Homebrew configuration
+  homebrew = {
+    enable = true;
+
+    brews = [
+      "mas"
+      "node"
+      "mongodb-atlas"
+    ];
+
+    casks = [
+      "spotify"
+      "prismlauncher"
+      "steam"
+      "mongodb-compass"
+    ];
+
+    masApps = {
+      # Uncomment and add as needed
+      # "Yoink" = 457622435;
+    };
+
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+      upgrade = true;
+    };
+  };
+
+  # User and system defaults
+  system = {
+    primaryUser = "iouhase";
+    configurationRevision = null;
+    stateVersion = 6;
+
+    defaults = {
+      dock = {
+        autohide = true;
+        enable-spring-load-actions-on-all-items = true;
+        minimize-to-application = true;
+        show-recents = true;
+        showhidden = true;
+        show-process-indicators = false;
+        
+        persistent-apps = [
+          { app = "/System//Applications/Mail.app"; }
+          { app = "/Applications/Nix Apps/Firefox.app"; }
+          { app = "/Applications/nix apps/discord.app"; }
+          { app = "/Applications/Spotify.app"; }
+          { app = "/System/Applications/Utilities/Terminal.app"; }
         ];
-
-      homebrew = {
-	enable = true;
-
-	brews = [
-	  "mas"
-	];
-
-	casks = [
-	  "spotify"
-	  "prismlauncher"
-	];
-
-	masApps = {
-	  #"Yoink" = 457622435;
-	};
-
-	onActivation.cleanup = "zap";
-	onActivation.autoUpdate = true;
-	onActivation.upgrade = true;
       };
+    };
+  };
 
-      system.primaryUser = "iouhase";
-      system.defaults = {
-	dock.autohide = true;
-	dock.enable-spring-load-actions-on-all-items = true;
-	dock.minimize-to-application = true;
-	dock.persistent-apps = [
-	{
-	 app = "/System//Applications/Mail.app";
-	}	
- 	{
-   	 app = "/Applications/Nix Apps/Firefox.app";
- 	}
-	{
-	 app = "/Applications/nix apps/discord.app";
-	}
-	{
-	 app = "/Applications/Spotify.app";
-	}	
-	{
-	 app = "/System/Applications/Utilities/Console.app";
-	}	
-	];
+  # Nix settings
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Shell configuration
+  programs = {
+    fish = {
+      enable = true;
+      shellAbbrs = {
+        nrb = "sudo darwin-rebuild switch --flake /etc/nix-darwin#laptop";
       };
+    };
+  };
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    }
-in
+  environment.shells = [ pkgs.fish ];
+}
--- flake.nix
+++ flake.nix
@@ -9,115 +9,19 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
-    configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-
-      nixpkgs.config.allowUnfree = true;	
-
-      environment.systemPackages =
-        [ 
-		pkgs.vim
-		pkgs.mkalias
-		pkgs.vscode
-		pkgs.firefox
-		#pkgs.spotify
-		pkgs.discord
-		pkgs.zip
-		pkgs.unzip
-		pkgs.neofetch
-        ];
-
-      homebrew = {
-	enable = true;
-
-	brews = [
-	  "mas"
-	  "node"
-	  "mongodb-atlas"
-	];
-
-	casks = [
-	  "spotify"
-	  "prismlauncher"
-	  "steam"
-	  "mongodb-compass"
-	];
-
-	masApps = {
-	  #"Yoink" = 457622435;
-	};
-
-	onActivation.cleanup = "zap";
-	onActivation.autoUpdate = true;
-	onActivation.upgrade = true;
-      };
-
-      system.primaryUser = "iouhase";
-      system.defaults = {
-	dock.autohide = true;
-	dock.enable-spring-load-actions-on-all-items = true;
-	dock.minimize-to-application = true;
-	dock.show-recents = true;
-	dock.showhidden = true;
-	dock.show-process-indicators = false;	
-	dock.persistent-apps = [
-	{
-	 app = "/System//Applications/Mail.app";
-	}	
- 	{
-   	 app = "/Applications/Nix Apps/Firefox.app";
- 	}
-	{
-	 app = "/Applications/nix apps/discord.app";
-	}
-	{
-	 app = "/Applications/Spotify.app";
-	}	
-	{
-	 app = "/System/Applications/Utilities/Terminal.app";
-	}	
-	];
-      };
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-      programs.fish.shellAbbrs = {
-	nrb = "sudo darwin-rebuild switch --flake /etc/nix-darwin#laptop";
-      };      
-
-      environment.shells = [ pkgs.fish ];
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#laptop
     darwinConfigurations."laptop" = nix-darwin.lib.darwinSystem {
       modules = [ 
-  configuration 
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-		nix-homebrew = {
-			enable = true;
-			enableRosetta = true;
-			user = "iouhase";
-		};
-	}
-	];
+        ./configuration.nix
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "iouhase";
+          };
+        }
+      ];
     };
   };
 }
